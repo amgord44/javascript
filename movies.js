dc398048dc8a3872a4a9a99c02133a3a2a9ec1ae
RV_Michelle Gordon <105226130+amgord44@users.noreply.github.com>
2022-05-27 18:05:33 -0400

    trying to add filter

--- movies.js
+++ movies.js
@@ -79,8 +79,8 @@
 
 }
 ]
-const year = movies.filter(e => e.date.includes('1950'));
-console.log(year);
+//const year = movies.filter(e => e.date.includes('1950'));
+//console.log(year);
 
 /*movies.forEach((e) => {
     console.log(`${e.title} ${e.date}`);*/
@@ -95,11 +95,24 @@ if(movies[i].date === 1950){*/
    // console.log(moveYear);
 
 
-
-//const button = document.getElementById('button');
-//button.addEventListener('click',() => {*/
-
-
-
+   function w3AddClass(element, name) {
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++) {
+      if (arr1.indexOf(arr2[i]) == -1) {
+        element.className += " " + arr2[i];
+      }
+    }
+  }
+const button = document.getElementById('btnContainer');
+const btns = btnContainer.getElementsByClassName(".btn");
+for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function(){
+      let current = document.getElementsByClassName(" btn active");
+      current[0].className = current[0].className.replace(" active", "");
+      this.className += " active";
+    });
+}
 
 
